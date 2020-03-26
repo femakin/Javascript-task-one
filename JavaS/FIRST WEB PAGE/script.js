e473bc28db27ed137b96abd0bb8749af4318ae5c
femakin <46872764+femakin@users.noreply.github.com>
2020-03-26 12:58:48 +0100

    Add files via upload

---
+++ JavaS/FIRST WEB PAGE/script.js
@@ -0,0 +1,11 @@
+// JS task 1
+// Store your name in a varibale. 
+// Store your courses in an array. 
+// Display your name and courses
+// If the number of courses you are doing is an odd number, find and display all odd numbers from 1-200 (inclusive)
+
+
+var name = "Omotosho Oluwatobiju Judah";
+
+var courses = ["HTML,CSS & JS", " NodeJS", " Design"];
+
---
+++ JavaS/js.js
@@ -0,0 +1,32 @@
+/* Task: 
+
+
+Store your name in a varibale. 
+
+Store your courses in an array. 
+
+Display your name and courses and do one of the following: 
+
+
+1. If the number of courses you are doing is an odd number,
+ find and display all odd numbers from 1-200 (inclusive)
+2. If the number of courses you are doing is an even number,
+ find and display all even numbers from 1-200 (inclusive)   */
+
+
+
+
+
+
+
+var name = 'AKINYEMI OLUWAFEMI TOSIN';
+console.log(name);
+var courses = ["HTML,CSS & JS"];
+console.log(courses);
+
+for (var i = 1; i < 200; i += 2) {
+	
+		console.log(i);
+	
+}
+
